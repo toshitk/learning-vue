8277ce9a9ae4ef1c59a37fa2b9cdd57729b667a9
toshitk <takahashi.t66@gmail.com>
2023-08-29 16:14:58 +0900

    feat: create toggle button

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <button @click="increment">count is: {{ count }}</button>
+  <input v-model="text" placeholder="Type here">
+  <button @click="toggle">toggle</button>
+  <h1 v-if="awesome">Vue is {{ text }}</h1>
+  <h1 v-else>Oh no 😢</h1>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
 const count = ref(0)
+const text = ref('')
+const awesome = ref(true)
 
 function increment() {
   count.value++
+}
+
+function toggle() {
+  awesome.value = !awesome.value
 }
 </script>
