2d769809d5b2cb2f317f00d56bc2ce4d08d4927f
toshitk <57493449+toshitk@users.noreply.github.com>
2023-08-30 09:39:31 +0900

    feat: watcher

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <button @click="toggle">toggle</button>
   <h1 v-if="awesome">Vue is {{ text }}</h1>
   <h1 v-else>Oh no 😢</h1>
-  
+
   <form @submit.prevent="addTodo">
     <input v-model="newTodo">
     <button>Add Todo</button>    
@@ -15,22 +15,29 @@
       <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>
+
+  <p>Todo id: {{ todoId }}</p>
+  <button @click="todoId++">Fetch next todo</button>
+  <p v-if="!todoData">Loading...</p>
+  <pre v-else>{{ todoData }}</pre>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 const count = ref(0)
 const text = ref('')
 const awesome = ref(true)
 let id = 0
-
 const newTodo = ref('')
 const todos = ref([
   { id: id++, text: 'Learn HTML' },
   { id: id++, text: 'Learn JavaScript' },
   { id: id++, text: 'Learn Vue' }
 ])
+const todoId = ref(1)
+const todoData= ref(null)
+
 function increment() {
   count.value++
 }
@@ -47,4 +54,14 @@ function addTodo() {
 function removeTodo(todo) {
   todos.value = todos.value.filter((t) => t !== todo)
 }
+
+async function fetchData() {
+  todoData.value = null
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+  )
+  todoData.value = await res.json()
+}
+fetchData()
+watch(todoId, fetchData)
 </script>
